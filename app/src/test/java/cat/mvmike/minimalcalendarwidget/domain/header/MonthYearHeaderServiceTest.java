43fbfd384b5020aa641cffc2ae0fb46134719ef5
Miquel Martí <miquelmarti111@gmail.com>
2020-12-13 20:08:10 +0100

    Refactor MonthYearHeaderServiceTest

--- app/src/test/java/cat/mvmike/minimalcalendarwidget/domain/header/MonthYearHeaderServiceTest.java
+++ app/src/test/java/cat/mvmike/minimalcalendarwidget/domain/header/MonthYearHeaderServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import android.widget.RemoteViews;
 
 import java.time.Instant;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Locale;
 import java.util.stream.Stream;
 
@@ -27,80 +28,50 @@ class MonthYearHeaderServiceTest extends BaseTest {
     private final RemoteViews widgetRv = mock(RemoteViews.class);
 
     @ParameterizedTest
-    @MethodSource("getSpreadInstantsInEnglish")
-    void setMonthYearHeader_shouldAddViewBasedOnCurrentMonthAndYearInEnglish(final Instant instant, final String expectedMonthAndYear) {
+    @MethodSource("getSpreadInstantsInDifferentLocales")
+    void setMonthYearHeader_shouldAddViewBasedOnCurrentMonthAndYear(final Instant instant,
+                                                                    final SimpleImmutableEntry<Locale, String> localeAndExpectedOutput) {
 
         when(systemResolver.getInstant()).thenReturn(instant);
-        when(systemResolver.getLocale(context)).thenReturn(Locale.ENGLISH);
+        when(systemResolver.getLocale(context)).thenReturn(localeAndExpectedOutput.getKey());
 
         setMonthYearHeader(context, widgetRv);
 
         verify(systemResolver, times(1)).getInstant();
         verify(systemResolver, times(1)).getLocale(context);
-        verify(systemResolver, times(1)).createMonthYearHeader(widgetRv, expectedMonthAndYear, 0.7f);
+        verify(systemResolver, times(1)).createMonthYearHeader(widgetRv, localeAndExpectedOutput.getValue(), 0.7f);
         verifyNoMoreInteractions(systemResolver);
     }
 
-    @ParameterizedTest
-    @MethodSource("getSpreadInstantsInCatalan")
-    void setMonthYearHeader_shouldAddViewBasedOnCurrentMonthAndYearInCatalan(final Instant instant, final String expectedMonthAndYear) {
-
-        when(systemResolver.getInstant()).thenReturn(instant);
-        when(systemResolver.getLocale(context)).thenReturn(new Locale("ca", "ES"));
-
-        setMonthYearHeader(context, widgetRv);
-
-        verify(systemResolver, times(1)).getInstant();
-        verify(systemResolver, times(1)).getLocale(context);
-        verify(systemResolver, times(1)).createMonthYearHeader(widgetRv, expectedMonthAndYear, 0.7f);
-        verifyNoMoreInteractions(systemResolver);
-    }
-
-    @ParameterizedTest
-    @MethodSource("getSpreadInstantsInRussian")
-    void setMonthYearHeader_shouldAddViewBasedOnCurrentMonthAndYearInRussian(final Instant instant, final String expectedMonthAndYear) {
-
-        when(systemResolver.getInstant()).thenReturn(instant);
-        when(systemResolver.getLocale(context)).thenReturn(new Locale("ru", "RU"));
-
-        setMonthYearHeader(context, widgetRv);
-
-        verify(systemResolver, times(1)).getInstant();
-        verify(systemResolver, times(1)).getLocale(context);
-        verify(systemResolver, times(1)).createMonthYearHeader(widgetRv, expectedMonthAndYear, 0.7f);
-        verifyNoMoreInteractions(systemResolver);
-    }
-
-    private static Stream<Arguments> getSpreadInstantsInEnglish() {
-
+    private static Stream<Arguments> getSpreadInstantsInDifferentLocales() {
+        Locale catalanLocale = new Locale("ca", "ES");
+        Locale russianLocale = new Locale("ru", "RU");
         return Stream.of(
-            Arguments.of(Instant.ofEpochMilli(896745600000L), "June 1998"),      // 1998-06-02 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1516924800000L), "January 2018"),  // 2018-01-26 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1804204800000L), "March 2027"),    // 2027-03-05 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1108771200000L), "February 2005"), // 2005-02-19 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1544659200000L), "December 2018")  // 2018-12-13 00:00 UTC
-        );
-    }
-
-    private static Stream<Arguments> getSpreadInstantsInCatalan() {
-
-        return Stream.of(
-            Arguments.of(Instant.ofEpochMilli(896745600000L), "Juny 1998"),     // 1998-06-02 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1516924800000L), "Gener 2018"),   // 2018-01-26 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1804204800000L), "Març 2027"),    // 2027-03-05 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1108771200000L), "Febrer 2005"),  // 2005-02-19 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1544659200000L), "Desembre 2018") // 2018-12-13 00:00 UTC
-        );
-    }
-
-    private static Stream<Arguments> getSpreadInstantsInRussian() {
-
-        return Stream.of(
-            Arguments.of(Instant.ofEpochMilli(896745600000L), "Июнь 1998"),     // 1998-06-02 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1516924800000L), "Январь 2018"),  // 2018-01-26 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1804204800000L), "Март 2027"),    // 2027-03-05 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1108771200000L), "Февраль 2005"), // 2005-02-19 00:00 UTC
-            Arguments.of(Instant.ofEpochMilli(1544659200000L), "Декабрь 2018")  // 2018-12-13 00:00 UTC
+            Arguments.of(Instant.ofEpochMilli(896745600000L), // 1998-06-02 00:00 UTC
+                    new SimpleImmutableEntry<>(Locale.ENGLISH, "June 1998"),
+                    new SimpleImmutableEntry<>(catalanLocale, "Juny 1998"),
+                    new SimpleImmutableEntry<>(russianLocale, "Июнь 1998")
+            ),
+            Arguments.of(Instant.ofEpochMilli(1516924800000L), // 2018-01-26 00:00 UTC
+                    new SimpleImmutableEntry<>(Locale.ENGLISH, "January 2018"),
+                    new SimpleImmutableEntry<>(catalanLocale, "Gener 2018"),
+                    new SimpleImmutableEntry<>(russianLocale, "Январь 2018")
+            ),
+            Arguments.of(Instant.ofEpochMilli(1804204800000L), // 2027-03-05 00:00 UTC
+                    new SimpleImmutableEntry<>(Locale.ENGLISH, "March 2027"),
+                    new SimpleImmutableEntry<>(catalanLocale, "Març 2027"),
+                    new SimpleImmutableEntry<>(russianLocale, "Март 2027")
+            ),
+            Arguments.of(Instant.ofEpochMilli(1108771200000L), // 2005-02-19 00:00 UTC
+                    new SimpleImmutableEntry<>(Locale.ENGLISH, "February 2005"),
+                    new SimpleImmutableEntry<>(catalanLocale, "Febrer 2005"),
+                    new SimpleImmutableEntry<>(russianLocale, "Февраль 2005")
+            ),
+            Arguments.of(Instant.ofEpochMilli(1544659200000L), // 2018-12-13 00:00 UTC
+                    new SimpleImmutableEntry<>(Locale.ENGLISH, "December 2018"),
+                    new SimpleImmutableEntry<>(catalanLocale, "Desembre 2018"),
+                    new SimpleImmutableEntry<>(russianLocale, "Декабрь 2018")
+            )
         );
     }
 }
