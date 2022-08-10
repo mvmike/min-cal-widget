997c10eb353f0917983e3f8c233cf59f98696692
Miquel Martí <miquelmarti111@gmail.com>
2022-08-10 21:35:12 +0200

    upgrade version - 2.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = namespace
         minSdk = minAndroidVersion
         targetSdk = androidVersion
-        versionCode = 44
-        versionName = "2.6.3"
+        versionCode = 45
+        versionName = "2.6.4"
 
         multiDexEnabled = true
     }
