5cf51c6f8fcb0da57d685492e778424efa1eac29
Miquel Martí <miquelmarti111@gmail.com>
2022-09-08 16:39:28 +0200

    simplify ProcessIntentUseCase

--- app/src/main/kotlin/cat/mvmike/minimalcalendarwidget/application/user/ProcessIntentUseCase.kt
+++ app/src/main/kotlin/cat/mvmike/minimalcalendarwidget/application/user/ProcessIntentUseCase.kt
@@ -4,7 +4,8 @@ package cat.mvmike.minimalcalendarwidget.application.user
 
 import android.content.Context
 import cat.mvmike.minimalcalendarwidget.application.RedrawWidgetUseCase
-import cat.mvmike.minimalcalendarwidget.domain.intent.ActionableView
+import cat.mvmike.minimalcalendarwidget.domain.intent.ActionableView.OPEN_CALENDAR
+import cat.mvmike.minimalcalendarwidget.domain.intent.ActionableView.OPEN_CONFIGURATION
 import cat.mvmike.minimalcalendarwidget.infrastructure.activity.CalendarActivity
 import cat.mvmike.minimalcalendarwidget.infrastructure.activity.ConfigurationActivity
 import cat.mvmike.minimalcalendarwidget.infrastructure.activity.PermissionsActivity
@@ -13,12 +14,10 @@ import cat.mvmike.minimalcalendarwidget.infrastructure.resolver.CalendarResolver
 
 object ProcessIntentUseCase {
 
-    fun execute(context: Context, action: String?) {
-        when (action) {
-            ActionableView.OPEN_CONFIGURATION.action -> context.executeAndRedrawOrAskForPermissions { ConfigurationActivity.start(context) }
-            ActionableView.OPEN_CALENDAR.action -> context.executeAndRedrawOrAskForPermissions { CalendarActivity.start(context, ClockConfig.getInstant()) }
-            else -> {}
-        }
+    fun execute(context: Context, action: String?) = when (action) {
+        OPEN_CONFIGURATION.action -> context.executeAndRedrawOrAskForPermissions { ConfigurationActivity.start(context) }
+        OPEN_CALENDAR.action -> context.executeAndRedrawOrAskForPermissions { CalendarActivity.start(context, ClockConfig.getInstant()) }
+        else -> {}
     }
 
     private fun Context.executeAndRedrawOrAskForPermissions(function: () -> Unit) =
