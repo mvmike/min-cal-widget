2490d2c24d6b4438c189724e3e69c7ee58aa3f2d
Miquel Martí <miquelmarti111@gmail.com>
2022-11-01 19:07:37 +0100

    upgrade gradle build tools - 7.4.0-beta04

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
 
     dependencies {
         //https://developer.android.com/studio/releases/gradle-plugin
-        classpath("com.android.tools.build:gradle:7.4.0-beta03")
+        classpath("com.android.tools.build:gradle:7.4.0-beta04")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 
