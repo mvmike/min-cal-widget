e30ce1fd4c78ee5b02433b5a5d7da13145c6bf50
Miquel Martí <miquelmarti111@gmail.com>
2022-11-29 17:48:25 +0100

    upgrade kotlin - 1.7.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 buildscript {
 
     //https://kotlinlang.org/releases.html
-    val kotlinVersion by extra { "1.7.21" }
+    val kotlinVersion by extra { "1.7.22" }
 
     dependencies {
         //https://developer.android.com/studio/releases/gradle-plugin
