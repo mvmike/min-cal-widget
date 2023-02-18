7eacaec4300cf18d62a007e150e1e8cfc3461362
Miquel Martí <miquelmarti111@gmail.com>
2023-02-18 21:03:55 +0100

    upgrade version - 2.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = namespace
         minSdk = minAndroidVersion
         targetSdk = androidVersion
-        versionCode = 58
-        versionName = "2.8.1"
+        versionCode = 59
+        versionName = "2.9.0"
 
         multiDexEnabled = true
     }
