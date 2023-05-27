4c019aa7852fd0f7e0f53b4686561ff08e55f244
Miquel Martí <miquelmarti111@gmail.com>
2023-05-27 19:58:45 +0200

    upgrade gradle build tools - 8.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ buildscript {
 
     dependencies {
         // https://developer.android.com/studio/releases/gradle-plugin
-        classpath("com.android.tools.build:gradle:8.0.1")
+        classpath("com.android.tools.build:gradle:8.0.2")
         // https://kotlinlang.org/releases.html
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
     }
