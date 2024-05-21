776846ac1364dd43efc6359bae4815a066c31b81
Miquel Martí <miquelmarti111@gmail.com>
2024-05-21 12:23:26 +0200

    add missing license

--- app/src/main/kotlin/cat/mvmike/minimalcalendarwidget/domain/configuration/MultilinePreference.kt
+++ app/src/main/kotlin/cat/mvmike/minimalcalendarwidget/domain/configuration/MultilinePreference.kt
@@ -1,3 +1,5 @@
+// Copyright (c) 2016, Miquel Martí <miquelmarti111@gmail.com>
+// See LICENSE for licensing information
 package cat.mvmike.minimalcalendarwidget.domain.configuration
 
 import android.content.Context
