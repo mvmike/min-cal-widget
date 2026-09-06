52d1720d708206b941dee0ddd0e81967eef79335
Miquel Martí <miquelmarti111@gmail.com>
2026-09-06 11:24:53 +0200

    Upgrade slf4j-simple - 2.0.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     // https://github.com/TNG/ArchUnit/releases
     testImplementation("com.tngtech.archunit:archunit-junit5:1.5.0")
     // https://github.com/qos-ch/slf4j/tags
-    testImplementation("org.slf4j:slf4j-simple:2.0.18")
+    testImplementation("org.slf4j:slf4j-simple:2.0.19")
 }
 
 // https://adoptium.net/temurin/releases/
